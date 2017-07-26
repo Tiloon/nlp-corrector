61b81a612275abe4d1265ea98ee6730dca15b75c
Hugo Rybinski <rybins_h@epita.fr>
2017-07-26 13:40:19 +0200

    fix freq set in sons

--- src/trie_node.cc
+++ src/trie_node.cc
@@ -115,50 +115,39 @@ void TrieNode::writeToBinaryFile(std::ofstream &of) {
 }
 
 void TrieNode::insert(std::string word, int freq) {
-//    std::cerr << "Insert |" << word << "| to node |" << prefix_ << "|" << std::endl;
+    std::cerr << "Insert |" << word << "| to node |" << prefix_ << "|" << std::endl;
     if (!word.length()) {
-//        std::cerr << "> Word end |" << word << "|" << std::endl;
+        std::cerr << "> Word end |" << prefix_ << "|" << std::endl;
+        freq_ = freq;
+        std::cerr << "> Word end |" << prefix_ << "|" <<  freq_ << std::endl;
+
         this->setWordEnd();
         return;
     }
 
     for (TrieNode& son : *(this->sons_)) {
-        for(int i = 0; i < word.length(); i++) { // FIXME changer le sens de la boucle ?
+        for(int i = 0; i < word.length(); i++) { // FIXME changer le sens de la boucle?
             std::string cutWord = word.substr(0, word.length() - i);  //word "abcd", cutWord "abc"
             if (cutWord.find(son.get_prefix()) == 0) { // ie: cutWord is "abc" and son is "ab"
-//                std::cerr << "> Going inside son |" << son.get_prefix() << "| with cutWord |" << cutWord << "|" << std::endl;
+                std::cerr << "> Going inside son |" << son.get_prefix() << "| with cutWord |" << cutWord << "|" << std::endl;
                 son.insert(word.substr(son.get_prefix().length()), freq);
                 return;
             }
             if (son.get_prefix().find(cutWord) == 0) { // ie: son is "abce" and cutWord is "abc"
-//                std::cerr << ">>>> Splitting son |" << son.get_prefix() << "| with cutWord |" << cutWord << "|" << std::endl;
-                TrieNode n = TrieNode(cutWord, 0, freq);
+                std::cerr << "> Splitting son |" << son.get_prefix() << "| with cutWord |" << cutWord << "|" << std::endl;
+                TrieNode n = TrieNode(cutWord, 0, 0);
                 n.sons_->push_back(son.removeFromPrefix(cutWord.length()));
                 this->sons_->erase(std::remove(this->sons_->begin(), this->sons_->end(), son), this->sons_->end());
-                this->sons_->push_back(n);
                 n.insert(word.substr(cutWord.length()), freq);
+                this->sons_->push_back(n);
+                std::cerr << "> Word end2 |" << n.prefix_ << "|" <<  n.freq_ << std::endl;
+
                 return;
             }
         }
-//        if (cutWord.find(son.get_prefix()) == 0) { // ie: word is "abc" and son is "ab"
-//            std::cerr << "> Going inside son " << son.get_prefix() << "with cutWord " << cutWord << std::endl;
-//            son.insert(word.substr(son.get_prefix().length()), freq);
-//            return;
-//        }
-//        if (son.get_prefix().find(cutWord) == 0) { // ie: son is "abc" and word is "ab"
-//            std::cerr << "> Splitting son" << son.get_prefix() << "with cutWord " << cutWord  << std::endl;
-//            TrieNode n = TrieNode(word, 1, freq);
-//            n.sons_.push_back(son.removeFromPrefix(word.length()));
-//            this->sons_.erase(std::remove(this->sons_.begin(), this->sons_.end(), son), this->sons_.end());
-//            this->sons_.push_back(n);
-//            return;
-//        }
     }
-//    std::cerr << "> New node |" << word << "| |" << prefix_ << "| sons size:" << sons_->size() << std::endl;
+    std::cerr << "> New node |" << word << "| |" << prefix_ << "|" << std::endl;
     this->sons_->emplace_back(word, 1, freq);
-//    std::cerr << "> New node |" << word << "| |" << prefix_ << "| sons size:" << sons_->size() << std::endl;
-
-
 }
 
 static int id_builder() {
@@ -169,10 +158,9 @@ static int id_builder() {
 void TrieNode::draw(std::ofstream& file, int id) {
     for (TrieNode& son : *(this->sons_)) {
         int id_next = id_builder();
-        std::cerr << "Draw " << son.prefix_  << " " << son.sons_->size() << std::endl;
-        file << "node" << id << " [label=\"" << prefix_ << "\", style=\"filled\", color=\"" <<
+        file << "node" << id << " [label=\"" << prefix_ << " (" << freq_ << ")" << "\", style=\"filled\", color=\"" <<
                 (isWordEnd_ ? "cadetblue" : "blue") << "\"]; node"
-        << id_next << " [label=\"" << son.prefix_  << " \", style=\"filled\", color=\"" <<
+        << id_next << " [label=\"" << son.prefix_  << " (" << son.freq_ << ")" << " \", style=\"filled\", color=\"" <<
                 (son.isWordEnd_ ? "cadetblue" : "blue")
         <<"\"]; node" << id << "-> node" << id_next << "\n";
         son.draw(file, id_next);
