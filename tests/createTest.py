e25dd86dcafc44fc51c17449a888d98f0b18a05a
Hugo Rybinski <rybins_h@epita.fr>
2017-07-27 16:34:56 +0200

    fix testsuite & add test creator
    Former-commit-id: 6cc4886ffde305b6bebba36c694c3ee74e832579

---
+++ tests/createTest.py
@@ -0,0 +1,20 @@
+from random import randrange, sample
+
+DICT = '../ref/words.txt'
+TESTS_DIR = './tests_files/'
+
+def createTest(fileName, nbWord, approx, lines):
+    linesPicked = sample(range(0, 100000), nbWord)
+    words = [lines[i].split('\t')[0] for i in linesPicked]
+    with open(TESTS_DIR + fileName, 'w+') as f:
+        for w in words:
+            f.write('approx ' + str(approx) + ' ' + w + '\n')
+
+    # with open(TESTS_DIR + fileName, 'w+'):
+
+with open(DICT) as f:
+    lines = f.readlines()
+    for approx in range(0, 4):
+        for nb in [10, 30, 100, 300, 1000, 3000]:
+            print('rand_' + str(nb) + '_' + str(approx) + '.txt')
+            createTest('rand_' + str(nb) + '_' + str(approx) + '.txt', nb, approx, lines)
--- tests/testSuite.py
+++ tests/testSuite.py
@@ -14,52 +14,80 @@ DICT = '../ref/words.txt'
 REF_DICT_BIN = '../ref/dict.bin'
 DICT_BIN = 'dict.bin'
 
-TEST_FILE = 'test.txt'
+TESTS_DIR = './tests_files'
 
 FNULL = open(os.devnull, 'w')
 
 
+def okko(test):
+    return '--> OK' if test else '--> KO'
+
+
+def test_memory(process):
+    ru = os.wait4(process.pid, 0)[2]
+    if (ru.ru_maxrss / 1024) > 512:
+        print(">> Maximum used memory: %d" % (ru.ru_maxrss / 1024), 'Mo', okko((ru.ru_maxrss / 1024) < 512))
+    return ru.ru_maxrss / 1024
 
 def test_comp():
     print('## COMPILER ##')
     start = time.time()
     p = Popen(['/usr/bin/time', '-v', COMP_BIN_PATH, DICT, DICT_BIN],  # ['/usr/bin/time', '-v', 'ls'],
                            stdout=PIPE, stderr=PIPE)
-    p.wait()
+    test_memory(p)
     print(">> Took: " + str(round(time.time() - start, 3)) + "s")
-    output, err = p.communicate()
-    memSize = int(err.decode('utf-8').split('\n')[-15].split(' ')[-1])
-    print(">> Maximum used memory (Mo): ", memSize // 1024)
+
+
+def search(filename, bin, dict_bin):
+    start = time.time()
+    ps = subprocess.Popen(['cat', filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    ps2 = subprocess.Popen([bin, dict_bin], stdin=ps.stdout, stdout=subprocess.PIPE, stderr=FNULL)
+    ps.wait()
+    memUsed = test_memory(ps2)
+    timeUsed = round(time.time() - start, 3)
+    _, err = ps.communicate()
+    output, _ = ps2.communicate()
+    # print(">> Took: " + str(timeUsed) + "s")
+    return output.decode('utf-8'), timeUsed, memUsed
 
 
 def test_search():
     print('## SEARCH ##')
-    start = time.time()
-    files = ['./tests_files/' + f for f in os.listdir('./tests_files') if os.path.isfile(os.path.join('./tests_files', f))]
+
+    #Stats
+    totalTest, passedTest, memoryGoodTest = 0, 0, 0
+
+    refTimeSum, mineTimeSum, diffTimeSum, percentTimeSum = 0, 0, 0, 0
+
+    files = [TESTS_DIR + f for f in os.listdir(TESTS_DIR) if os.path.isfile(os.path.join('./tests_files', f))]
     for filename in files:
         if filename[-4:] != '.txt':
             continue
+        totalTest += 1
         print('# File: ', filename)
 
-        ps = subprocess.Popen(['cat', filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output = subprocess.check_output([APP_BIN_PATH, DICT_BIN], stdin=ps.stdout)
-        ps.wait()
-        _, err = ps.communicate()
-        print(output, err)
-        mine = output.decode('utf-8')
-        # memSize = int(err.decode('utf-8').split('\n')[-15].split(' ')[-1])
-        # print(">> Maximum used memory (Mo): ", memSize // 1024)
+        outMine, timeMine, memUsed = search(filename, APP_BIN_PATH, DICT_BIN)
+        if memUsed < 512:
+            memoryGoodTest += 1
 
+        outRef, timeRef, _ = search(filename, REF_APP_BIN_PATH, REF_DICT_BIN)
+        # print(outRef)
+        if outRef != outMine:
+            print('!! Diff with ref: ', okko(outRef == outMine))
+        else:
+            passedTest += 1
+        refTimeSum += timeRef
+        mineTimeSum += timeMine
+        diffTimeSum += round(timeMine - timeRef, 3)
+        percentTimeSum += round(timeMine / timeRef, 2) * 100
+        print('>> Time analysis: Ref', timeRef, 's | Ours', timeMine, 's | Difference',
+              round(timeMine - timeRef, 3), 's | Percent perf', round(timeMine / timeRef, 2) * 100, '%')
 
-        ps = subprocess.Popen(['cat', filename], stdout=subprocess.PIPE, stderr=FNULL)
-        output = subprocess.check_output([REF_APP_BIN_PATH, REF_DICT_BIN], stdin=ps.stdout, stderr=FNULL)
-        ps.wait()
-        ref = output.decode('utf-8')
-        # print(ref)
-        # print(mine)
-        print('>> Diff with ref: ', 'KO' if mine != ref else 'OK')
-
-
+    print('\n#########\n# RECAP #\n#########')
+    print('>> Passed test:', passedTest, '/', totalTest, '(', 100 * passedTest // totalTest, '%)')
+    print('>> Memory passed test:', memoryGoodTest, '/', totalTest, '(', 100 * memoryGoodTest // totalTest, '%)')
+    print('>> Average Time analysis: Ref', round(refTimeSum / totalTest, 3), 's | Ours', round(mineTimeSum / totalTest, 3),
+          's | Difference', round(diffTimeSum / totalTest, 3), 's | Percent perf', round(percentTimeSum / totalTest, 2), '%')
 
 # test_comp()
 test_search()
