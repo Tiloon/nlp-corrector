6968b04dfcbca3d974eaff769df7849583fb61f5
Tilon <denis.casteran@gmail.com>
2017-07-29 01:10:41 +0200

    fix compilation
    Former-commit-id: 938d9c19a0ea074ab2fe3d2b693e320aec8a1dbf

--- src/bin_node.cc
+++ src/bin_node.cc
@@ -4,22 +4,22 @@
 
 #include "bin_node.hh"
 
-inline const char *get_son(const char *ptr, size_t len) {
+const char *get_son(const char *ptr, size_t len) {
     return ptr + len + 1 + sizeof(long) * 2;
 }
 
-inline const char *get_brother(const char *start, const char *ptr, size_t len) {
+const char *get_brother(const char *start, const char *ptr, size_t len) {
     return start + *(long *) (ptr + len + 1 + sizeof(long));
 }
 
-inline const char *BinNode::g_son(const char *ptr, size_t len) {
+const char *BinNode::g_son(const char *ptr, size_t len) {
     return get_son(ptr, len);
 }
 
-inline const char *BinNode::g_brother(const char *ptr, size_t len) {
+const char *BinNode::g_brother(const char *ptr, size_t len) {
     return get_brother(this->start, ptr, len);
 }
 
-inline const char *BinNode::go_to(size_t len) {
+const char *BinNode::go_to(size_t len) {
     return start + len;
 }
